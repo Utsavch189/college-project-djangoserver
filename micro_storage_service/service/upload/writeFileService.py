478c29f5648e90631a7e02ad44b9aaf36d9eaa8c
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-08-15 10:54:07 +0530

    update writeFileService

--- micro_storage_service/service/upload/writeFileService.py
+++ micro_storage_service/service/upload/writeFileService.py
@@ -2,12 +2,10 @@ from micro_storage_service.utils.upload.decode import Decode
 from micro_storage_service.DTO.upload.writeobjDto import WriteFileDTO
 
 class WriteFileService:
-    def __init__(self) -> None:
-        self._decode:Decode=Decode()
 
     def write(self,dto:WriteFileDTO)->bool:
         try:
-            decoded_dict:dict=self._decode.decoding(music_obj=dto.uploaddto.music_fileobj,musicCover_obj=dto.uploaddto.music_cover_fileobj)
+            decoded_dict:dict=Decode.decoding(music_obj=dto.uploaddto.music_fileobj,musicCover_obj=dto.uploaddto.music_cover_fileobj)
             with open(f'media/{dto.uploaddto.music_filename}','wb') as f:
                 f.write(decoded_dict['decoded_music_obj'])
                 f.close()
