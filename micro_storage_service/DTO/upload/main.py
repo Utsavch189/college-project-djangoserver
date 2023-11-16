a7c4f3b8610dae95cb4dff71ab50a72e13443e7e
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-11-16 14:45:01 +0530

    update

--- micro_storage_service/DTO/upload/main.py
+++ micro_storage_service/DTO/upload/main.py
@@ -7,7 +7,7 @@ class UploadAPI(BaseModel):
     music_cover_filename:constr(min_length=1,strip_whitespace=True)
     music_cover_fileobj:constr(min_length=1,strip_whitespace=True)
     artist_name:constr(min_length=1,strip_whitespace=True)
-    desc:constr(min_length=1,strip_whitespace=True)
+    desc:constr(strip_whitespace=True)=""
 
     @validator('music_fileobj',allow_reuse=True,always=True)
     def arrange_musicFileObj(cls,value):
